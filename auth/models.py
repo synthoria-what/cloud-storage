702e354b8422933cd57ff76a79541a1551b8a8f7
Мурсалимов Тимур Салаватович <22200322@live.preco.ru>
2025-10-16 09:49:25 +0500

    migrated auth

--- auth/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-
-class User(BaseModel):
-    username: str = Field(description="Логин пользователя")
-    password: str = Field(description="Пароль пользователя")
-    email: str = Field(default=None, description="Почта пользователя")
-    role: str = Field(default="user", description="Роль пользователя")
